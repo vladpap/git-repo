ad32a4a4420b3e3879a67961e5ffedae45d09877
vladpap <vladimir.papin@gmail.com>
2016-03-16 11:40:49 +0300

    Создание класса Hello world, с точкой входа в программу

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Error");
+			System.exit(0);
+		}
+		int result = 0;
+		for (int i = 0; i < args[0].length(); i++) {
+			char ch = args[0].charAt(i);
+			result = result * 10 + (ch - '0');
+		}
+		System.out.println("Result = " + result * 2);
+		
+	}
+}
